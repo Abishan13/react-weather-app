4ca080837ead45d354e4544d0a557e0658b4df61
Abishan13 <arul.abishanpro@gmail.com>
2023-01-10 16:56:05 +0100

    Version 1.0

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Footer() {
+  return (
+
+
+    <footer class="p-9 rounded-lg shadow md:px-6 md:py-2 ">
+      <div class="sm:flex sm:items-center sm:justify-between">
+        <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+        </a>
+
+      </div>
+      <hr class="my-2 opacity-25 sm:mx-auto lg:my-8" />
+      <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a class="">WeatheReact</a>. All Rights Reserved.
+      </span>
+    </footer>
+
+
+  )
+}
+
+export default Footer;
